e96ccf1cb2251400066fa70716cd4c26c47e104a
nnarct <nannapat1551@gmail.com>
2024-05-15 18:11:11 +0700

    chore: add todo in comment

--- client/src/components/PostListItem.tsx
+++ client/src/components/PostListItem.tsx
@@ -1,16 +1,26 @@
-import { Button } from "antd"
-import { Link } from "react-router-dom"
+import { Button } from "antd";
+import { Link } from "react-router-dom";
 
-
-const PostListItem = ({postID} : {postID: string}) => {
+const PostListItem = ({ postID }: { postID: string }) => {
   // get postTitle, petPic, petType, petAge to be displayed
+
+  if (!postID) {
+    return null;
+  }
+
+  // Todo : one list should have 2 actions:
+  // 1. see post url : /myposts/pdetail/:postID
+  // 2. see post answer (submission from other users) url: /myposts/pdetail/answerslist/:postID
+  // 3. delete post // link to nothing [ just ui button , not functional]
 
   return (
     <>
-        <Link to="/myposts/pdetail" state={{ postID: postID }}>   <Button> click to see example post id {postID}</Button>
-      </Link> {/* Implement PostListItem Here */}
+      <Link to={`/myposts/pdetail/${postID}`}>
+        <Button> This is list example {postID}</Button>
+      </Link>
+      {/* Implement PostListItem Here */}
     </>
-  )
-}
+  );
+};
 
-export default PostListItem
+export default PostListItem;
