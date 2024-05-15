2a92cadc1f6b940cb1b88997f16dda4bac8219c6
nnarct <nannapat1551@gmail.com>
2024-05-15 17:24:18 +0700

    feat: key EN ,value TH

---
+++ client/src/assets/translate.ts
@@ -0,0 +1,7 @@
+export const PATH_NAMES: { [key: string]: string } = {
+  profile: "ข้อมูลส่วนตัว",
+  myposts: "ประกาศของฉัน",
+  editpf: "แก้ไขข้อมูลส่วนตัว",
+  pdetail: "รายละเอียดโพสต์",
+  petdetails: "รายละเอียดสัตว์เลี้ยง",
+};
